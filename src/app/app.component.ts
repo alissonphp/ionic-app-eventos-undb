55e4669857686b6dacab976f6477ca02250a2f21
Alisson Gomes <alisson.php@gmail.com>
2021-05-27 23:51:03 -0300

    ref(chore): Adjust initial project setup - colors, routes, template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IEvent } from './events/event.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  events: IEvent[];
+  constructor() {
+    this.events = [
+      {
+        title: 'Ionic Week - Zero to Mobile Hero',
+        date: 'May 25, 9h30 PM',
+        speaker: 'Mr. Elon Musk',
+        done: true,
+        participants: [
+          {
+            fullname: 'Alisson Gomes',
+            document: '123188293192',
+            birth: '08/07/1991',
+            email: 'alisson@alissongomes.com',
+          },
+        ],
+      },
+      {
+        title: 'UX JAM - Knowing what your client think',
+        date: 'May 26, 5h30 PM',
+        speaker: 'Liza Simpson',
+        done: false,
+        participants: [],
+      },
+      {
+        title: 'Web apps Vs. Native apps',
+        date: 'May 27, 2h30 PM',
+        speaker: 'Sir. Bruce Wayne',
+        done: false,
+        participants: [],
+      },
+      {
+        title: 'Introduction to Capacitor integrations',
+        date: 'May 28, 1h30 PM',
+        speaker: 'Mr. Edson Yanaga',
+        done: false,
+        participants: [],
+      },
+    ];
+    localStorage.setItem('events', JSON.stringify(this.events));
+  }
 }
